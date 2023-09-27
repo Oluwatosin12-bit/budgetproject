a42fce2885cf7db87b2c6dad7b4308b6b03e9879
Oluwatosin12-bit <85936439+Oluwatosin12-bit@users.noreply.github.com>
2023-09-26 19:26:54 -0500

    Update settings.py

--- budgetproject/settings.py
+++ budgetproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
@@ -120,7 +119,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = BASE_DIR / 'static',
 STATIC_ROOT = 'staticfiles'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
